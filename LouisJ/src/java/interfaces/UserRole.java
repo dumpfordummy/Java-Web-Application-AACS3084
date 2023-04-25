bdf7a74834e10a66eb8badfc2c4f5df012c5b054
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2023-04-25 16:50:15 +0800

    userrole

---
+++ LouisJ/src/java/interfaces/UserRole.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author CY
+ */
+public abstract class UserRole {
+    static final String CUSTOMER = "customer";
+    static final String STAFF = "staff";
+    static final String MANAGER = "manager";
+    String role;
+}
--- LouisJ/src/java/util/UserSessionUtil.java
+++ LouisJ/src/java/util/UserSessionUtil.java
@@ -4,9 +4,37 @@
  */
 package util;
 
+import javax.servlet.http.*;
+import model.Customer;
+
 /**
  *
  * @author CY
  */
 public class UserSessionUtil {
+    
+    private HttpSession session;
+    private final String SESSION_ID = "sessionId";
+
+    public UserSessionUtil(HttpSession session) {
+        this.session = session;
+    }
+    
+    // need to set this cookie returned to user's cookie
+    public Cookie setSessionCustomer(Customer customer) {
+        String sessionId = session.getId();
+        session.setAttribute(sessionId, customer);
+        return new Cookie(SESSION_ID,sessionId);
+    }
+    
+    public Customer getSessionCustomer(Cookie[] userCookies) {
+        for(Cookie cookie : userCookies) {
+            if(cookie.getName().equals(SESSION_ID)) {
+                return (Customer)session.getAttribute(cookie.getValue());
+            }
+        }
+        //Whenever user logged in, session and cookie will be set, null indicates that it is not logged in.
+        return null;
+    }
+    
 }
