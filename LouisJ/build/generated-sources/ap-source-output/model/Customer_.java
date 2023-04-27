49589a66e8e694786859f0de625df6d8051e440f
YEUNG KIEN XIN <kienxin90@gmail.com>
2023-04-27 22:17:42 +0800

    welcome page

--- LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-04-27T22:15:32")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-04-27T22:17:28")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
