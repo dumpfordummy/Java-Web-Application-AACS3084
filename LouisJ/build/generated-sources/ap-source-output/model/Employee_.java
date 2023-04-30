1abcdf9adbde804b9bafcb12e9e9fb4e17046fdd
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2023-04-30 23:48:30 +0800

    update sql

--- LouisJ/build/generated-sources/ap-source-output/model/Employee_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Employee_.java
@@ -1,22 +1,22 @@
 package model;
 
-import java.io.Serializable;
 import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-04-30T22:05:52")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-04-30T23:14:52")
 @StaticMetamodel(Employee.class)
 public class Employee_ { 
 
     public static volatile SingularAttribute<Employee, String> address;
     public static volatile SingularAttribute<Employee, String> contact;
+    public static volatile SingularAttribute<Employee, String> profileimgtype;
     public static volatile SingularAttribute<Employee, String> usertype;
     public static volatile SingularAttribute<Employee, String> fullname;
     public static volatile SingularAttribute<Employee, String> id;
     public static volatile SingularAttribute<Employee, String> passwordhash;
-    public static volatile SingularAttribute<Employee, Serializable> profileimg;
+    public static volatile SingularAttribute<Employee, String> profileimg;
     public static volatile SingularAttribute<Employee, Date> datejoin;
     public static volatile SingularAttribute<Employee, String> email;
     public static volatile SingularAttribute<Employee, String> username;
