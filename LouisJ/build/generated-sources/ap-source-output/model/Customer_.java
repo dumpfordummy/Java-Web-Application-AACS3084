148061197f331f32bfe571f66056341d9a8f4834
Pua8 <87104240+Pua8@users.noreply.github.com>
2023-05-02 20:16:03 +0800

    welcome page

--- LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
@@ -5,8 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-02T15:21:43")
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-02T15:38:59")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-02T17:31:19")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
