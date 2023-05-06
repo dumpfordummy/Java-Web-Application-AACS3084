af784e3737418e31c211dd23b9332e66805b3788
Pua8 <87104240+Pua8@users.noreply.github.com>
2023-05-06 18:37:57 +0800

    dropdown on account

--- LouisJ/build/generated-sources/ap-source-output/model/Cart_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Cart_.java
@@ -4,8 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T16:14:18")
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T15:05:53")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T18:08:57")
 @StaticMetamodel(Cart.class)
 public class Cart_ { 
 
--- LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Customer_.java
@@ -5,8 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T16:14:18")
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T15:05:53")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T18:08:57")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
