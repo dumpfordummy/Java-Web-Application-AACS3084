7a408ca5b37729f92004ad1c3343d8dfbb28670b
YEUNG KIEN XIN <kienxin90@gmail.com>
2023-05-06 21:39:24 +0800

    ?????

--- LouisJ/build/generated-sources/ap-source-output/model/Product_.java
+++ LouisJ/build/generated-sources/ap-source-output/model/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T21:32:37")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-06T21:38:26")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
